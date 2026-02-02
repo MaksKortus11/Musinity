10b680dc295a069094c7594cc0ea9ebdf13c47fd
MaksKortus11 <adiseba11@gmail.com>
2026-02-02 11:07:03 +0100

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,1 +1,11 @@
+export default function Login() {
+  return (
+    <div className="center">
+      <h2>Logowanie</h2>
+      <input placeholder="Username" />
+      <input type="password" placeholder="Hasło" />
+      <button>Zaloguj</button>
+    </div>
+  );
+}
 
