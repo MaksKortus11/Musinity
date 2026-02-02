735852ea6be4076d72145d4b42e47d6fd2e1e7c0
MaksKortus11 <adiseba11@gmail.com>
2026-02-02 11:05:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,35 @@
+import { Routes, Route, useLocation } from "react-router-dom";
+import AnimatedBackground from "./background/AnimatedBackground";
+import LandingLayout from "./layout/LandingLayout";
+import AppLayout from "./layout/AppLayout";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Chat from "./pages/Chat";
+import Events from "./pages/Events";
+
+export default function App() {
+  const { pathname } = useLocation();
+  const showBg = pathname === "/" || pathname === "/login";
+
+  return (
+    <>
+      {showBg && <AnimatedBackground />}
+
+      <Routes>
+        <Route element={<LandingLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
+
+        <Route element={<AppLayout />}>
+          <Route path="/chat" element={<Chat />} />
+          <Route path="/events" element={<Events />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
