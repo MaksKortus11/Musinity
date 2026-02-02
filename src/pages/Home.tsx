8dba82242e7620606184e570e29d0246d6451f01
MaksKortus11 <adiseba11@gmail.com>
2026-02-02 11:06:50 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,16 @@
+import { Link } from "react-router-dom";
+
+export default function Home() {
+  return (
+    <div className="center">
+      <h2>Musinity</h2>
+      <p>Your music community</p>
+
+      <div className="actions">
+        <Link to="/login">Zaloguj się</Link>
+        <Link to="/register">Załóż konto</Link>
+      </div>
+    </div>
+  );
+}
 
