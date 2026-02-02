8ad2a55d4f49e26a3cd37f44ccabd488da2183e7
MaksKortus11 <adiseba11@gmail.com>
2026-02-02 11:06:28 +0100

    Update AppLayout.tsx

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,1 +1,20 @@
+import { Outlet, NavLink } from "react-router-dom";
+
+export default function AppLayout() {
+  return (
+    <div className="app">
+      <aside className="sidebar">
+        <h1>Musinity</h1>
+        <nav>
+          <NavLink to="/chat">Chat</NavLink>
+          <NavLink to="/events">Eventy</NavLink>
+        </nav>
+      </aside>
+
+      <main className="main">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
 
