ea5fddce9a5db15b3af38ec4197012dd521f97e4
MaksKortus11 <adiseba11@gmail.com>
2026-02-02 11:05:51 +0100

    Update AnimatedBackground.tsx

--- src/background/AnimatedBackground.tsx
+++ src/background/AnimatedBackground.tsx
@@ -1,1 +1,18 @@
+import Dither from "./Dither";
+
+export default function AnimatedBackground() {
+  return (
+    <div className="bg-layer">
+      <Dither
+        waveColor={[0.06, 0.24, 0.18]}
+        waveAmplitude={0.12}
+        waveFrequency={3.0}
+        waveSpeed={0.015}
+        colorNum={6}
+        disableAnimation={false}
+        enableMouseInteraction={false}
+      />
+    </div>
+  );
+}
 
